169cdc894f37f2943d8e3bfc71d7c6f942f55538
C.mirkin <ciromirkin@gmail.com>
2012-01-01 00:19:41 -0300

    cambiado: la logica que genera el tablero de juego inicial y las fichas separadas en una funcion

--- src/index.js
+++ src/index.js
@@ -2,23 +2,27 @@
 
 const tableGame = document.getElementById('tableGame')
 
-const tableFragment = document.createDocumentFragment()
-let isRedCell = false
-for (let y = 0; y < 10; y++) {
-    const row = document.createElement('UL')
-    row.classList.add('row')
-    
-    for (let x = 0; x < 10; x++) {
-        const cell = document.createElement('LI')
-        cell.classList.add('cell')
-        cell.classList.add(isRedCell ? 'cell--red' : 'cell--black')
-        if(isRedCell && (y < 4 || y > 5)) {
-            cell.innerHTML = `<div class="index-card index-card${y < 4 ? '--black' : '--red'}"></div>`
+const getInitialTableGame = () => {
+    const tableFragment = document.createDocumentFragment()
+    let isRedCell = false
+    for (let y = 0; y < 10; y++) {
+        const row = document.createElement('UL')
+        row.classList.add('row')
+        
+        for (let x = 0; x < 10; x++) {
+            const cell = document.createElement('LI')
+            cell.classList.add('cell')
+            cell.classList.add(isRedCell ? 'cell--red' : 'cell--black')
+            if(isRedCell && (y < 4 || y > 5)) {
+                cell.innerHTML = `<div class="index-card index-card${y < 4 ? '--black' : '--red'}"></div>`
+            }
+            isRedCell = !isRedCell
+            row.appendChild(cell)
         }
         isRedCell = !isRedCell
-        row.appendChild(cell)
+        tableFragment.appendChild(row)
     }
-    isRedCell = !isRedCell
-    tableFragment.appendChild(row)
+    return tableFragment
 }
-tableGame.appendChild(tableFragment)
+
+tableGame.appendChild(getInitialTableGame())
