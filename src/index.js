56e90623e4d48c6b026072f52301bbfe35d6c8c3
C.mirkin <ciromirkin@gmail.com>
2012-01-01 00:54:19 -0300

    agregado: identificadores para las filas y celdas

--- src/index.js
+++ src/index.js
@@ -2,20 +2,47 @@
 
 const tableGame = document.getElementById('tableGame')
 
+const getTableIds = (long) => {
+    long = long || 10
+    const letters = [...'abcdefghij']
+
+    return letters.map(letter => {
+        const cellsIds = []
+        const rowId = `${letter}-row` 
+
+        for(let i =0; i < long; i++) {
+            cellsIds.push(`${letter}${i}-cell`)
+        }
+
+        return {
+            rowId,
+            cellsIds
+        }
+    })
+}
+
 const getInitialTableGame = () => {
     const tableFragment = document.createDocumentFragment()
     let isRedCell = false
+    const tableGameIds = getTableIds()
+
     for (let y = 0; y < 10; y++) {
         const row = document.createElement('UL')
         row.classList.add('row')
+        row.setAttribute('id', tableGameIds.at(y).rowId)
         
         for (let x = 0; x < 10; x++) {
             const cell = document.createElement('LI')
+            const cellId = tableGameIds.at(y).cellsIds.at(x)
+
+            cell.setAttribute('id', cellId)
             cell.classList.add('cell')
             cell.classList.add(isRedCell ? 'cell--red' : 'cell--black')
+
             if(isRedCell && (y < 4 || y > 5)) {
                 cell.innerHTML = `<div class="index-card index-card${y < 4 ? '--black' : '--red'}"></div>`
             }
+            
             isRedCell = !isRedCell
             row.appendChild(cell)
         }
