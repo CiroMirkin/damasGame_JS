6df8c7a47fe87668a6a364e3bc5291333e9de499
C.mirkin <ciromirkin@gmail.com>
2012-01-01 01:03:36 -0300

    creado: ahora el tablero es creado desde javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 "use strinct"
+
+const table = document.getElementById('table')
+
+const tableFragment = document.createDocumentFragment()
+let isRedCell = false
+for (let y = 0; y < 10; y++) {
+    const row = document.createElement('UL')
+    row.classList.add('row')
+    
+    for (let x = 0; x < 10; x++) {
+        const cell = document.createElement('LI')
+        cell.classList.add('cell')
+        cell.classList.add(isRedCell ? 'cell--red' : 'cell--black')
+        row.appendChild(cell)
+        isRedCell = !isRedCell
+    }
+    isRedCell = !isRedCell
+    tableFragment.appendChild(row)
+}
+table.appendChild(tableFragment)
